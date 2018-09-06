6f4fea81c448d6f0bd40b8ed9a3c56d91d01ef9f
Neto Lucena <juracylucena@gmail.com>
2018-09-06 11:41:30 -0300

    Tutorial Inicial

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
+import {increment, increment_amount, decrement, zero} from "../redux/index"
+
 class Counter extends Component {
   render() {
     return (
       <div>
-        <h1>1</h1>
+        <h1>{this.props.counter}</h1>        
+        <button onClick = { () => this.props.increment() }> Increment </button>
+        <button onClick = { () => this.props.increment_amount(10) }> Increment Amount 10</button>
+        <button onClick = { () => this.props.increment_amount(20) }> Increment Amount 20</button>
+        <button onClick = { () => this.props.zero() }> Zero </button>
+        <button onClick = { () => this.props.decrement() }> Decrement </button>                
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  
+  counter: state.counter
 });
 
 const mapDispatchToProps = {
-  
+  increment, 
+  increment_amount,
+  decrement, 
+  zero
 };
 
 export default connect(
